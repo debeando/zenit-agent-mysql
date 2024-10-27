b819c5829cc46f94afa4f21141f8369ee3096cd1
Nicola Strappazzon <nicola@strappazzon.me>
2024-10-27 11:14:50 +0100

    fix - go format

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "time"
+	"time"
 
-    "github.com/debeando/go-common/env"
-    "github.com/debeando/go-common/log"
-    "github.com/debeando/go-common/mysql"
+	"github.com/debeando/go-common/env"
+	"github.com/debeando/go-common/log"
+	"github.com/debeando/go-common/mysql"
 )
 
 var Debug string
@@ -18,41 +18,41 @@ var MySQLTimeout uint8
 var MySQLUser string
 
 func init() {
-    Debug = env.Get("DEBUG", "true")
-    Interval = time.Duration(env.GetInt("INTERVAL", 3)) * time.Second
-    MySQLHost = env.Get("MYSQL_HOST", "127.0.0.1")
-    MySQLPassword = env.Get("MYSQL_PASSWORD", "monitoring")
-    MySQLPort = env.GetUInt16("MYSQL_PORT", 3306)
-    MySQLTimeout = env.GetUInt8("MYSQL_TIMEOUT", 10)
-    MySQLUser = env.Get("MYSQL_USER", "monitoring")
-    MySQLDSN = (&mysql.MySQL{
-        Host: MySQLHost,
-        Password: MySQLPassword,
-        Port: MySQLPort,
-        Timeout: MySQLTimeout,
-        Username: MySQLUser,
-    }).DSN()
+	Debug = env.Get("DEBUG", "true")
+	Interval = time.Duration(env.GetInt("INTERVAL", 3)) * time.Second
+	MySQLHost = env.Get("MYSQL_HOST", "127.0.0.1")
+	MySQLPassword = env.Get("MYSQL_PASSWORD", "monitoring")
+	MySQLPort = env.GetUInt16("MYSQL_PORT", 3306)
+	MySQLTimeout = env.GetUInt8("MYSQL_TIMEOUT", 10)
+	MySQLUser = env.Get("MYSQL_USER", "monitoring")
+	MySQLDSN = (&mysql.MySQL{
+		Host:     MySQLHost,
+		Password: MySQLPassword,
+		Port:     MySQLPort,
+		Timeout:  MySQLTimeout,
+		Username: MySQLUser,
+	}).DSN()
 
-    if Debug == "true" {
-        log.SetLevel(log.DebugLevel)
-    }
+	if Debug == "true" {
+		log.SetLevel(log.DebugLevel)
+	}
 }
 
-func main() {    
-    log.Info("Start DeBeAndo Zenit Agent for MySQL")
-    log.DebugWithFields("Environment Variables", log.Fields{
-            "DEBUG": Debug,
-            "INTERVAL": Interval,
-            "MYSQL_HOST": MySQLHost,
-            "MYSQL_PORT": MySQLPort,
-            "MYSQL_USER": MySQLUser,
-            "MYSQL_PASSWORD": MySQLPassword,
-            "MYSQL_TIMEOUT": MySQLTimeout,
-    })
+func main() {
+	log.Info("Start DeBeAndo Zenit Agent for MySQL")
+	log.DebugWithFields("Environment Variables", log.Fields{
+		"DEBUG":          Debug,
+		"INTERVAL":       Interval,
+		"MYSQL_HOST":     MySQLHost,
+		"MYSQL_PORT":     MySQLPort,
+		"MYSQL_USER":     MySQLUser,
+		"MYSQL_PASSWORD": MySQLPassword,
+		"MYSQL_TIMEOUT":  MySQLTimeout,
+	})
 
-    for {
-        CollectStatus()
-        log.Debug("Wait until next collect metrics.")
-        time.Sleep(Interval)
-    }
+	for {
+		CollectStatus()
+		log.Debug("Wait until next collect metrics.")
+		time.Sleep(Interval)
+	}
 }
